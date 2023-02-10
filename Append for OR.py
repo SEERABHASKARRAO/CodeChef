9b2007f224a83bac4e5e92121d7dd908fc503cd7
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-10 21:54:48 +0530

    Create Append for OR.py

---
+++ Append for OR.py
@@ -0,0 +1,16 @@
+# cook your dish here
+for _ in range(int(input())):
+        n,y=map(int,input().split())
+        l=list(map(int,input().split()))
+        res=l[0]
+        for i in range(1,len(l)):
+            res=res|l[i]
+        if(res==y):
+            print(0)
+        else:
+            t=y-res
+            if(res|t==y):
+                print(t)
+            else:
+                print(-1)
+            
