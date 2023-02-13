2f53116236ff4dc24fd31d4e492fb449ff3943be
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-13 16:40:04 +0530

    Update Motivation.py

--- Motivation.py
+++ Motivation.py
@@ -6,7 +6,7 @@ for _ in range(int(input())):
         a,b=map(int,input().split())
         s.append([a,b])
     s.sort(reverse=True,key=operator.itemgetter(1))
-    maxi=0 
+    maxi=0
     for i in range(len(s)):
         if(s[i][0]<=x):
             maxi=max(maxi,s[i][1])
