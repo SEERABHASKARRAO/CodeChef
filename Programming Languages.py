82c295e2af97cece9bae7bf57f7dfbf20184f271
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-14 16:43:07 +0530

    Update Programming Languages.py

--- Programming Languages.py
+++ Programming Languages.py
@@ -1,6 +1,6 @@
 # cook your dish here
 for _ in range(int(input())): 
-    a,b,a1,b1,a2,b2=map(int,input().split()) 
+    a,b,a1,b1,a2,b2=map(int,input().split())
     if((a==b1 or a==a1)and (b==b1 or b==a1)):
         print(1)
     elif((a==b2 or a==a2)and (b==b2 or b==a2)):
