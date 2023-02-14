94df1658f5366e34c688711cb2d10b958bc115f2
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-14 17:00:23 +0530

    Update Programming Languages.py

--- Programming Languages.py
+++ Programming Languages.py
@@ -4,6 +4,6 @@ for _ in range(int(input())):
     if((a==b1 or a==a1)and (b==b1 or b==a1)):
         print(1)
     elif((a==b2 or a==a2)and (b==b2 or b==a2)):
-        print(2)
+        print(2) 
     else:
         print(0)
