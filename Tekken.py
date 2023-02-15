5f9258108c2247b3d5701f109ef4c18483440bc2
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-15 22:07:32 +0530

    Create Tekken.py

---
+++ Tekken.py
@@ -0,0 +1,16 @@
+# cook your dish here
+for _ in range(int(input())):
+        a,b,c=map(int,input().split())
+        t=min(b,c)
+        b=b-t
+        c=c-t
+        t=min(a,c)
+        a=a-t
+        c=c-t
+        t=min(a,b)
+        a=a-t
+        b=b-t
+        if(a>0):
+            print("YES")
+        else:
+            print("NO")
