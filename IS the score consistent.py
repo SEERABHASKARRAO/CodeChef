31ce20cbd990587369dffa18759a1fe75831c52f
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-16 18:37:52 +0530

    Create IS the score consistent.py

---
+++ IS the score consistent.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    a,b=map(int,input().split())
+    c,d=map(int,input().split())
+    if(a>c or b>d):
+        print("IMPOSSIBLE")
+    else:
+        print("POSSIBLE")
