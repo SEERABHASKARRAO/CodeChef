59b955b6a5b71c9222da9a8583675edde9e70aa5
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-21 16:39:24 +0530

    Create The Great Run.py

---
+++ The Great Run.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    n,a=map(int,input().split())
+    l=list(map(int,input().split()))
+    maxi=-99999
+    for i in range(n-1):
+        sum1=sum(l[i:i+a])
+        maxi=max(maxi,sum1)
+    print(maxi)
+    
