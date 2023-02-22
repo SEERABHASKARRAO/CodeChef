b6f24459b889c87abe4168029ad0a0d7323ace6a
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-22 21:34:24 +0530

    Create Self Defence Training.py

---
+++ Self Defence Training.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().split()))
+    c=0
+    for i in l:
+        if(i>=10 and i<=60):
+            c+=1 
+    print(c)
