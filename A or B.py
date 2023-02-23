e4d153287d6100525e40b12615e4166878eac793
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-23 21:18:36 +0530

    Create A or B.py

---
+++ A or B.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    x,y=map(int,input().split())
+    s1=500-(x*2)
+    s2=1000-((x+y)*4)
+    r1=s1+s2
+    t1=1000-(y*4)
+    t2=500-((x+y)*2)
+    r2=t1+t2
+    print(max(r1,r2))
+    
