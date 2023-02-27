b2a6cb7c10c960a430dafd79ff0916e6f08abf59
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-02-27 18:27:52 +0530

    Create ATM Machine.py

---
+++ ATM Machine.py
@@ -0,0 +1,13 @@
+
+for _ in range(int(input())):
+    n,k=map(int,input().split())
+    l=list(map(int,input().split()))
+    p=''
+    sum1=0
+    for i in l:
+        if(i<=k):
+            k-=i
+            p+='1'
+        else:
+            p+='0'
+    print(p)
