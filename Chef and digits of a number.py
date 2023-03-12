8a47f6739bc4dbf7cc357ec3069a9001783e7906
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-03-12 18:51:27 +0530

    Create Chef and digits of a number.py

---
+++ Chef and digits of a number.py
@@ -0,0 +1,8 @@
+# cook your dish here
+for _ in range(int(input())):
+    s=input()
+    
+    if(list(s).count('0')==1 or list(s).count('1')==1 ):
+        print("Yes")
+    else:
+        print("No")
