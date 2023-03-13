e6b24d9badde61d57a9026c24b84bcf38c416501
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-03-13 19:17:39 +0530

    Create Racing Horses.py

---
+++ Racing Horses.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n=int(input())
+    l=list(map(int,input().split()))
+    l.sort()
+    b=[]
+    for i in range(n-1):
+        b.append(abs(l[i]-l[i+1]))
+    print(min(b))
