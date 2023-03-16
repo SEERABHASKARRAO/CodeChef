d1684dadf73d70768cf776bb3806f5704f7c6fc5
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-03-16 19:34:08 +0530

    Create Vaccine Distribution.py

---
+++ Vaccine Distribution.py
@@ -0,0 +1,20 @@
+# cook your dish here
+t = int(input())
+while t:
+    n,d=map(int,input().split())
+    a=list(map(int,input().split()))
+    c=0
+    for i in range(n):
+        if a[i]<=9 or a[i]>=80:
+            c +=1
+    g=n-c
+    if c%d==0:
+        d1=c//d
+    else:
+        d1=c//d+1
+    if g%d==0:
+        d2=g//d
+    else:
+        d2=g//d+1    
+    print(d1+d2)    
+    t -= 1
