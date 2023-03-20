f1801a0ff558f5b242ff90263da918fb200be3ae
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-03-20 19:13:57 +0530

    Create Snake Procession.py

---
+++ Snake Procession.py
@@ -0,0 +1,27 @@
+t=int(input())
+while t!=0:
+    n=int(input())
+    s=input()
+    l=[]
+    for i in s:
+        if i=='H' or i=='T':
+            l.append(i)
+    l=''.join(l)
+    if len(l)==0:
+        print("Valid")
+    else:
+        k=l.count('HT')
+        if len(l)%2==0:
+            if k==len(l)//2:
+                print("Valid")
+            else:
+                print("Invalid")
+        else:
+            if k==len(l)//2+1:
+                print("Valid")
+            else:
+                print("Invalid")
+    
+    
+    t-=1
+           
