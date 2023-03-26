a1eff3de48b78368c14c1ae908ab01e35fb21cd6
SEERABHASKARRAO <108410439+SEERABHASKARRAO@users.noreply.github.com>
2023-03-26 11:52:14 +0530

    Create Processing a string.py

---
+++ Processing a string.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    s=input()
+    s=list(s)
+    sum1=0
+    t=[]
+    for i in s:
+        if(ord(i)>=48 and ord(i)<=57):
+            sum1+=int(i)
+    print(sum1)
